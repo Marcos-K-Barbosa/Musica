cec98cd058fe340141e9e252c39e327ca0830aac
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-14 19:47:07 -0300

    Programa de Música executado.

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Aleluia ( Quem Muito Foi Perdoado )");
+Thread.Sleep(4000);
+Console.ResetColor();
+Console.WriteLine("O grande amor de Deus");
+Console.WriteLine("O Rei que me formou");
+Console.WriteLine("Das trevas me tirou");
+Thread.Sleep(4000);
+Console.WriteLine("A água que sacia");
+Console.WriteLine("O pão que vem do céu");
+Console.WriteLine("Jesus mudou minha vida");
+Thread.Sleep(4000);
+Console.WriteLine("Cantarei");
+Thread.Sleep(5000);
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Ale, aleluia");
+Console.WriteLine("Ale, aleluia");
+Console.WriteLine("Fui perdoado");
+Console.WriteLine("Amado por Jesus");
+Console.WriteLine("Fui perdoado");
+Console.ResetColor();
+Thread.Sleep(4000);
+Console.WriteLine("A água que sacia");
+Console.WriteLine("O pão que vem do céu");
+Console.WriteLine("Jesus mudou minha vida");
+Thread.Sleep(4000);
+Console.WriteLine("Cantarei");
+Thread.Sleep(4000);
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Ale, aleluia");
+Console.WriteLine("Ale, aleluia");
+Console.WriteLine("Fui perdoado");
+Console.WriteLine("E Amado por Jesus");
+Console.WriteLine("Fui perdoado");
+Console.WriteLine("Aleluia");
+Thread.Sleep(3000);
+Console.WriteLine("Ale, aleluia");
+Console.WriteLine("Ale, aleluia");
+Console.WriteLine("Fui perdoado");
+Console.WriteLine("E Amado por Jesus");
+Console.WriteLine("Fui perdoado");
+Console.ResetColor();
+Thread.Sleep(4000);
+Console.WriteLine("Quem muito foi");
+Thread.Sleep(1000);
+Console.WriteLine("Quem muito foi perdoado, muito ama");
+Console.WriteLine("Quem muito foi perdoado, muito ama");
+Console.WriteLine("E eu, eu te amo, meu Rei e Salvador");
+Console.WriteLine("Eu te amo");
+Console.WriteLine("Quem muito foi");
+Thread.Sleep(3000);
+Console.WriteLine("Quem muito foi perdoado, muito ama");
+Console.WriteLine("Quem muito foi perdoado, muito ama");
+Console.WriteLine("E eu, eu te amo, meu Rei e Salvador");
+Thread.Sleep(3000);
+Console.WriteLine("Os Seus olhos são como pedras preciosas");
+Console.WriteLine("Olhando pra mim");
+Console.WriteLine("Os Seus olhos são como pedras preciosas");
+Console.WriteLine("Olhando pra mim");
+Thread.Sleep(1000);
+Console.WriteLine("Os Seus olhos são como pedras preciosas");
+Console.WriteLine("Olhando pra mim");
+Console.WriteLine("Os Seus olhos são como pedras preciosas");
+Console.WriteLine("Olhando pra mim");
+Thread.Sleep(1000);
+Console.WriteLine("Os Seus olhos são como pedras preciosas");
+Console.WriteLine("Olhando pra mim");
+Console.WriteLine("Os Seus olhos são como pedras preciosas");
+Console.WriteLine("Olhando pra mim");
+Thread.Sleep(1000);
+Console.WriteLine("Os Seus olhos são como pedras preciosas");
+Console.WriteLine("Olhando pra mim");
+Console.WriteLine("Os Seus olhos são como pedras preciosas");
+Console.WriteLine("Olhando pra mim");
+Thread.Sleep(1000);
+Console.WriteLine("E nós queremos ver");
+Console.WriteLine("Os Teus olhos como chama");
+Console.WriteLine("E nós queremos ver");
+Console.WriteLine("Os Teus olhos como chama");
+Thread.Sleep(3000);
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine(@"Aleluia
+Ale, aleluia
+Ale, aleluia
+Fui perdoado
+E amado por Jesus
+Fui perdoado
+Quem muito foi");
+Console.ResetColor();
+Thread.Sleep(2000);
+Console.WriteLine(@"Quem muito foi perdoado, muito ama
+Quem muito foi perdoado, muito ama
+E eu, eu te amo, meu Rei e Salvador
+Eu te amo");
+Console.WriteLine(@"Os Seus olhos são como pedras preciosas
+Olhando pra mim
+Os Seus olhos são como pedras preciosas
+Olhando pra mim");
